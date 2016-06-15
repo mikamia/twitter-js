ef709522a9adbf0f6bae66495b04ba90f22d6ccb
Ayako Mikami <ayakomikami11@gmail.com>
2016-06-15 14:55:21 -0400

    up to git collab

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.use(function (req, res, next) {
+    console.log('application level status code', res.statusCode);
+    next();
+});
+
+app.use('/special', function (req, res, next) {
+    console.log('special application level');
+    next();
+})
+
+
+app.get('/', function(req, res){
+  res.send('hi');
+  console.log('GET');
+
+});
+
+
+app.listen(3000);
+console.log("server listening....");

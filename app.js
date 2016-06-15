5b7ef72e80d536cff1dfc460091e6696a6aa17ad
Ayako Mikami <ayakomikami11@gmail.com>
2016-06-15 16:16:40 -0400

    working on users/username

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 var express = require('express');
 var swig = require('swig');
 var app = express();
+var routes = require('./routes/');
+
+app.use('/', routes);
 
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views/');
 swig.setDefaults({ cache: false });
 
-app.use(function (req, res, next) {
-    console.log('application level status code', res.statusCode);
-    next();
-});
-
-app.use('/special', function (req, res, next) {
-    console.log('special application level');
-    next();
-})
+// app.use(function (req, res, next) {
+//     console.log('application level status code', res.statusCode);
+//     next();
+// });
 
 
-app.get('/', function(req, res){
-  var people = [{name: 'Ayako'}, {name: 'Winnie'}, {name: 'Chocolate'}];
-  res.render( 'index', {title: 'Hall of Fame', people: people} );
-  //res.render('index.html', locals);
-  // swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
-  //   res.send(output);
-  // });
-  console.log('GET');
+// app.get('/', function(req, res){
+//   var people = [{name: 'Ayako'}, {name: 'Winnie'}, {name: 'Chocolate'}];
+//   res.render( 'index', {title: 'Hall of Fame', people: people} );
+//   //res.render('index.html', locals);
+//   // swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
+//   //   res.send(output);
+//   // });
+//   console.log('GET');
 
-});
+// });
 
 
 app.listen(3000);
---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+// could use one line instead: var router = require('express').Router();
+var tweetBank = require('../tweetBank');
+
+router.use(express.static('public'));
+
+// router.get('/stylesheets/style.css', function(req, res){
+//   res.sendFile('../stylesheets/style.css');
+// });
+
+router.get('/', function (req, res) {
+  var tweets = tweetBank.list();
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+});
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  res.render( 'index', { title: 'Twitter.js - Posts by '+name, list: list } );
+});
+
+module.exports = router;

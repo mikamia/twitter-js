dfd56221aa6a47920ad234d342d58d61250a587d
Ayako Mikami <ayakomikami11@gmail.com>
2016-06-15 15:28:19 -0400

    rendered template

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require('express');
 var swig = require('swig');
 var app = express();
 
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views/');
+swig.setDefaults({ cache: false });
+
 app.use(function (req, res, next) {
     console.log('application level status code', res.statusCode);
     next();
@@ -14,7 +19,12 @@ app.use('/special', function (req, res, next) {
 
 
 app.get('/', function(req, res){
-  res.send('hi');
+  var people = [{name: 'Ayako'}, {name: 'Winnie'}, {name: 'Chocolate'}];
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
+  //res.render('index.html', locals);
+  // swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
+  //   res.send(output);
+  // });
   console.log('GET');
 
 });
@@ -22,3 +32,14 @@ app.get('/', function(req, res){
 
 app.listen(3000);
 console.log("server listening....");
+
+// var locals = {
+//     title: 'An Example',
+//     people: [
+//         { name: 'Gandalf'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'}
+//     ]
+// };
+
+

001abb5cb8286676eec961c6438f9ec3597ddc6c
Ayako Mikami <ayakomikami11@gmail.com>
2016-06-15 17:06:58 -0400

    up to bonus

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,40 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
 
 router.use(express.static('public'));
 
+router.use(bodyParser.urlencoded({ extended: false}))
+
+router.use(bodyParser.json())
 // router.get('/stylesheets/style.css', function(req, res){
 //   res.sendFile('../stylesheets/style.css');
 // });
 
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm:true } );
 });
 
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
   var tweets = tweetBank.find( {name: name} );
-  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: tweets } );
+  res.render( 'index', { title: 'Twitter.js - Posts by '+name, user: name, tweets: tweets, showForm: true } );
 });
 
 router.get('/tweets/:id', function(req, res){
-  var userID = Number(req.params.id);
-  var tweets = tweetBank.find({id: userID});
-  res.render( 'index', { title: 'Twitter.js - Posts by '+userID, tweets: tweets } );
+  var tweetID = Number(req.params.id);
+  var tweets = tweetBank.find({id: tweetID});
+  res.render( 'index', { title: 'Twitter.js - Posts by '+tweetID, tweets: tweets, showForm: false } );
+});
+
+router.post('/tweets', function(req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
 });
 
 module.exports = router;
